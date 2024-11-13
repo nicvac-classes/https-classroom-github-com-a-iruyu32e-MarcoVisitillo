b98966fb356e067c2234a06ac026a43c458b408f
MarcoVisitillo <158757207+MarcoVisitillo@users.noreply.github.com>
2024-11-13 19:18:22 +0000

    Consegna

--- backend/app.js
+++ backend/app.js
@@ -2,37 +2,61 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const app = express();
+const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
+const FileStore = require('session-file-store')(session);
 
 // Configurazione EJS
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    store: new FileStore({
+        path: './sessions', 
+        ttl: 86400         
+    }),
+    secret: 'la mia chiave segreta',  
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        maxAge: 24 * 60 * 60 * 1000  
+    }
+}));
 
 app.get('/', (req, res) => {
-    const name = req.cookies.name; // Legge il cookie "name"
-    if (name) {
-        // Se il cookie esiste, mostra la pagina di saluto
-        res.render('greet', { message:'Bentornato', name: name });
+    if (req.session.name) {
+        res.render('greet', { 
+            message: 'Bentornato', 
+            name: req.session.name 
+        });
     } else {
-        // Se non esiste, mostra il form
         res.render('form');
     }
 });
 
 app.post('/greet', (req, res) => {
     const name = req.body.name;
-    // Imposta un cookie chiamato "name" con valore l'input dell'utente
-    res.cookie('name', name, { maxAge: 24 * 60 * 60 * 1000 }); 
-    res.render('greet', { message:'Benvenuto', name: name });
+    
+    // Salviamo il nome nella sessione
+    req.session.name = name;
+    
+    // Generiamo un ID di sessione casuale
+    req.session.id = uuidv4();
+    
+    res.render('greet', { 
+        message: 'Benvenuto', 
+        name: name 
+    });
 });
 
 app.post('/logout', (req, res) => {
-
-    //Cancella tutti i cookies
-    Object.keys(req.cookies).forEach(cookie => res.clearCookie(cookie));
-
-    res.redirect('/'); // Reindirizza alla home page
+    req.session.destroy((err) => {
+        if(err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 const PORT = 3000;
